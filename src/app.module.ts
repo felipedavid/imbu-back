05bb16c5e412681f4b9fea043608d6129718c5fa
Felipe David <felipedavid.huh@gmail.com>
2024-11-05 21:34:04 -0300

    adding logs to requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { AuthController } from './auth/auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './users/models/users.schema';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  }),
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {target: 'pino-pretty'},
+      }
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
